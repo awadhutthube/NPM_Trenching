ea7a8fc76ef06baef1788543cefd1e73064d5cba
awadhutthube <awadhutthube@gmail.com>
2020-05-29 15:53:49 -0400

    Tested first algorithm

--- process.py
+++ process.py
@@ -9,8 +9,9 @@ import sensor_msgs.point_cloud2 as pcl2
 import std_msgs
 from scipy.spatial.transform import Rotation as R
 import matplotlib.pyplot as plt
-plt.ion()
+import tf
 
+plt.ion()
 pub1 = rospy.Publisher('/cloud_transformed', PointCloud2, queue_size=5)
 pub2 = rospy.Publisher('/cloud_original', PointCloud2, queue_size=5)
 
@@ -24,17 +25,24 @@ def get_transformation_matrix(tvec, quat):
     transformation_matrix[:3,-1] = tvec
     return transformation_matrix
 
-def plot_histogram(datapoints, number, start = -0.03, end = 0.08, interval_size = 0.0005):
+def generate_histogram(datapoints, number, start = -0.03, end = 0.08, interval_size = 0.0005, draw = False):
     num = (end-start)//interval_size
     bin_sequence = np.linspace(start, end, num)
-    axes = plt.gca()
-    axes.set_ylim([0,7000])
-    axes.set_xlim([-0.03,0.08])
-    plt.hist(datapoints, bin_sequence)
-    plt.savefig('../histograms/bag_1/frame_' + str(number))
-    plt.show()
-    plt.pause(0.005)
-    plt.clf()
+    if draw:    
+        plt.figure(1); plt.clf()
+        axes = plt.gca()
+        axes.set_ylim([0,7000])
+        axes.set_xlim([-0.03,0.08])
+        data, bins, _ = plt.hist(datapoints, bin_sequence)
+        plt.savefig('../histograms/bag_4/frame_' + str(number))
+    else:
+        data, bins = np.histogram(datapoints, bin_sequence) 
+    return data, bins
+    
+def get_trench_threshold(hist_data, hist_bins):
+    max_idx = np.argmax(hist_data)
+    threshold = hist_bins[max_idx-4,]
+    return threshold
 
 def read_rosbag(filepath):
     bag = rosbag.Bag(bagfile_path, 'r')
@@ -44,30 +52,63 @@ def read_rosbag(filepath):
         transformed_xyz = transform_cloud(xyz, H)
         mean_xyz = np.mean(transformed_xyz, axis = 0)
         transformed_xyz -= mean_xyz
-        # plot_histogram(transformed_xyz[:,2], idx)
-        heightmap = generate_heightmap(transformed_xyz)
-        # visualize_heightmap(heightmap)
-        # publish_transformed_cloud(transformed_xyz)
+        data, bins = generate_histogram(transformed_xyz[:,2], idx, draw = False)
+        trench_thresh = get_trench_threshold(data, bins)
+        print("Trench threshold is {}".format(trench_thresh))
+        # mask_height(transformed_xyz, trench_thresh)
+        heightmap = generate_heightmap(transformed_xyz, trench_thresh, mask = True)
+        visualize_heightmap(heightmap, idx)        
+        publish_transformed_cloud(transformed_xyz)
         # publish_transformed_cloud(xyz, False)
         print(idx)
     return
 
-def generate_heightmap(points, dim = 500):
-    heightmap = np.zeros((dim,dim)).astype('float')
+def publish_threshold_frame(threshold):
+    br = tf.TransformBroadcaster()
+    br.sendTransform((0,0,threshold),
+                     tf.transformations.quaternion_from_euler(0, 0, 0),
+                     rospy.Time.now(),
+                     "threshold_frame",
+                     "map")
+    return
+
+def generate_heightmap(points, threshold, dim = 500, mask = False):
+    heightmap = np.zeros((dim,dim,3)).astype('float')
     x_co = (points[:,0]*1000).round().astype('int')
     y_co = (points[:,1]*1000).round().astype('int')
     z_co = points[:,2]
     x_co -= np.amin(x_co)
     y_co -= np.amin(y_co)
-    z_co -= np.amin(z_co)
-    print(z_co)
-    heightmap[x_co,y_co] = z_co
+    # min_val = np.amin(z_co)
+    # z_co -= min_val
+    # threshold -= min_val
+    print(threshold)
+    publish_threshold_frame(threshold)
+    heightmap[x_co,y_co,0] = 1
+    if mask:
+        masked_x_co = x_co[z_co < threshold]
+        masked_y_co = y_co[z_co < threshold]
+        heightmap[masked_x_co, masked_y_co, 1] = 1
+    
     return heightmap
 
-def visualize_heightmap(heightmap):
-    plt.figure(1)
+# def mask_height(heightmap, threshold):
+#     masked_points = points[points[:,2] < threshold-0.01]
+#     print("Shape of masked points {}".format(masked_points.shape))
+#     # masked_points = masked_points[masked_points[:,2] > threshold-0.001]
+#     print(masked_points.shape)
+#     masked_map = generate_heightmap(masked_points)
+#     plt.figure(3)
+#     plt.clf()
+#     plt.imshow(masked_map, cmap = 'gray')
+#     plt.pause(0.0002)
+#     # visualize_heightmap(masked_map)
+
+def visualize_heightmap(heightmap, number):
+    plt.figure(2)
     plt.clf()
-    plt.imshow(heightmap, cmap = 'gray')
+    plt.imshow(heightmap)
+    plt.savefig('../heightmaps/bag_4/frame_' + str(number))
     plt.pause(0.0002)
     return
 
@@ -82,6 +123,7 @@ def publish_transformed_cloud(cloud_array, flag = True):
     else:
         cloud = pcl2.create_cloud_xyz32(header,cloud_array)
         pub1.publish(cloud)
+    return
 
 def transform_cloud(cloud_array, transformation_matrix = np.eye(4)):
     if cloud_array.shape[0] > cloud_array.shape[1]:
@@ -95,8 +137,8 @@ def transform_cloud(cloud_array, transformation_matrix = np.eye(4)):
 if __name__ == '__main__':
     rospy.init_node('cloud_processing_node')
     # bagfile_path = '../test_data/01_K10MINI_2020-05-14-16-23-00.bag'
-    bagfile_path = '../test_data/02_K10MINI_2020-05-14-16-31-57.bag'
+    # bagfile_path = '../test_data/02_K10MINI_2020-05-14-16-31-57.bag'
     # bagfile_path = '../test_data/03_K10MINI_2020-05-14-16-44-14.bag'
-    # bagfile_path = '../test_data/04_K10MINI_2020-05-14-16-49-59.bag'
+    bagfile_path = '../test_data/04_K10MINI_2020-05-14-16-49-59.bag'
     read_rosbag(bagfile_path)
     print("Processing Complete")
