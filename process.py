f3f10868787f35d17c65671d9e9bc9aeaa72686f
awadhutthube <awadhutthube@gmail.com>
2020-05-29 16:22:35 -0400

    Separated utils file

--- process.py
+++ process.py
@@ -7,9 +7,9 @@ import ros_numpy
 import rospy
 import sensor_msgs.point_cloud2 as pcl2
 import std_msgs
-from scipy.spatial.transform import Rotation as R
+
 import matplotlib.pyplot as plt
-import tf
+import utils
 
 plt.ion()
 pub1 = rospy.Publisher('/cloud_transformed', PointCloud2, queue_size=5)
@@ -18,58 +18,35 @@ pub2 = rospy.Publisher('/cloud_original', PointCloud2, queue_size=5)
 tvec = [-0.16927510039335014, -0.3717676310584948, -0.0332700755223688+1]
 quat = [0.9378418721923328, 0.1422734925170896, -0.04777733124311015, -0.31293482182246196]
 
-def get_transformation_matrix(tvec, quat):
-    transformation_matrix = np.eye(4)
-    r = R.from_quat(quat)
-    transformation_matrix[:3,:3] = r.as_dcm()
-    transformation_matrix[:3,-1] = tvec
-    return transformation_matrix
 
-def generate_histogram(datapoints, number, start = -0.03, end = 0.08, interval_size = 0.0005, draw = False):
+def generate_histogram(datapoints, frame_idx, start = -0.03, end = 0.08, interval_size = 0.0005, draw = False):
     num = (end-start)//interval_size
     bin_sequence = np.linspace(start, end, num)
     if draw:    
-        plt.figure(1); plt.clf()
-        axes = plt.gca()
-        axes.set_ylim([0,7000])
-        axes.set_xlim([-0.03,0.08])
-        data, bins, _ = plt.hist(datapoints, bin_sequence)
-        plt.savefig('../histograms/bag_4/frame_' + str(number))
+        data, bins = utils.visualize_histogram(datapoints, bin_sequence, frame_idx)
     else:
         data, bins = np.histogram(datapoints, bin_sequence) 
     return data, bins
     
 def get_trench_threshold(hist_data, hist_bins):
     max_idx = np.argmax(hist_data)
-    threshold = hist_bins[max_idx-4,]
+    threshold = hist_bins[max_idx-4]
     return threshold
 
 def read_rosbag(filepath):
     bag = rosbag.Bag(bagfile_path, 'r')
     for idx, (topic, msg, t) in enumerate(bag.read_messages(topics=['/sensor/side_camera/depth/color/points'])):
         xyz = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(msg, remove_nans=True)
-        H = get_transformation_matrix(tvec, quat)
+        H = utils.get_transformation_matrix(tvec, quat)
         transformed_xyz = transform_cloud(xyz, H)
         mean_xyz = np.mean(transformed_xyz, axis = 0)
         transformed_xyz -= mean_xyz
         data, bins = generate_histogram(transformed_xyz[:,2], idx, draw = False)
         trench_thresh = get_trench_threshold(data, bins)
-        print("Trench threshold is {}".format(trench_thresh))
-        # mask_height(transformed_xyz, trench_thresh)
         heightmap = generate_heightmap(transformed_xyz, trench_thresh, mask = True)
-        visualize_heightmap(heightmap, idx)        
+        utils.visualize_heightmap(heightmap, idx)        
         publish_transformed_cloud(transformed_xyz)
-        # publish_transformed_cloud(xyz, False)
-        print(idx)
-    return
-
-def publish_threshold_frame(threshold):
-    br = tf.TransformBroadcaster()
-    br.sendTransform((0,0,threshold),
-                     tf.transformations.quaternion_from_euler(0, 0, 0),
-                     rospy.Time.now(),
-                     "threshold_frame",
-                     "map")
+        utils.log_data(idx, trench_thresh)
     return
 
 def generate_heightmap(points, threshold, dim = 500, mask = False):
@@ -82,35 +59,13 @@ def generate_heightmap(points, threshold, dim = 500, mask = False):
     # min_val = np.amin(z_co)
     # z_co -= min_val
     # threshold -= min_val
-    print(threshold)
-    publish_threshold_frame(threshold)
+    utils.publish_threshold_frame(threshold)
     heightmap[x_co,y_co,0] = 1
     if mask:
         masked_x_co = x_co[z_co < threshold]
         masked_y_co = y_co[z_co < threshold]
-        heightmap[masked_x_co, masked_y_co, 1] = 1
-    
+        heightmap[masked_x_co, masked_y_co, 1] = 1    
     return heightmap
-
-# def mask_height(heightmap, threshold):
-#     masked_points = points[points[:,2] < threshold-0.01]
-#     print("Shape of masked points {}".format(masked_points.shape))
-#     # masked_points = masked_points[masked_points[:,2] > threshold-0.001]
-#     print(masked_points.shape)
-#     masked_map = generate_heightmap(masked_points)
-#     plt.figure(3)
-#     plt.clf()
-#     plt.imshow(masked_map, cmap = 'gray')
-#     plt.pause(0.0002)
-#     # visualize_heightmap(masked_map)
-
-def visualize_heightmap(heightmap, number):
-    plt.figure(2)
-    plt.clf()
-    plt.imshow(heightmap)
-    plt.savefig('../heightmaps/bag_4/frame_' + str(number))
-    plt.pause(0.0002)
-    return
 
 def publish_transformed_cloud(cloud_array, flag = True):
     header = std_msgs.msg.Header()
@@ -141,4 +96,5 @@ if __name__ == '__main__':
     # bagfile_path = '../test_data/03_K10MINI_2020-05-14-16-44-14.bag'
     bagfile_path = '../test_data/04_K10MINI_2020-05-14-16-49-59.bag'
     read_rosbag(bagfile_path)
+    plt.ioff()
     print("Processing Complete")
---
+++ utils.py
@@ -0,0 +1,45 @@
+from scipy.spatial.transform import Rotation as R
+import tf
+import matplotlib.pyplot as plt
+import numpy as np
+import rospy
+
+def get_transformation_matrix(tvec, quat):
+    transformation_matrix = np.eye(4)
+    r = R.from_quat(quat)
+    transformation_matrix[:3,:3] = r.as_dcm()
+    transformation_matrix[:3,-1] = tvec
+    return transformation_matrix
+
+def publish_threshold_frame(threshold):
+    br = tf.TransformBroadcaster()
+    br.sendTransform((0,0,threshold),
+                     tf.transformations.quaternion_from_euler(0, 0, 0),
+                     rospy.Time.now(),
+                     "threshold_frame",
+                     "map")
+    return
+
+def visualize_heightmap(heightmap, number):
+    plt.figure(2)
+    plt.clf()
+    plt.imshow(heightmap)
+    plt.savefig('../heightmaps/bag_4/frame_' + str(number))
+    plt.pause(0.002)
+    return
+
+def visualize_histogram(datapoints, bin_sequence, f_idx):
+    plt.figure(1); plt.clf()
+    axes = plt.gca()
+    axes.set_ylim([0,7000])
+    axes.set_xlim([-0.03,0.08])
+    data, bins, _ = plt.hist(datapoints, bin_sequence)
+    plt.savefig('../histograms/bag_4/frame_' + str(f_idx))
+    plt.pause(0.002)
+    return data, bins
+
+def log_data(f_idx, trench_threshold):
+    print("Frame index is {}".format(f_idx))
+    print("Trench threshold = {}".format(trench_threshold))
+    print("----------------------------------------------")
+    return
