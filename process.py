3805ed16f5f2316f4bd39a0bfc91a6ccd625b986
awadhutthube <awadhutthube@gmail.com>
2020-06-01 19:00:02 -0400

    Made minor changes in heightmap

--- process.py
+++ process.py
@@ -1,4 +1,3 @@
-
 import rosbag
 import numpy as np
 from sensor_msgs.msg import PointCloud2
@@ -30,7 +29,7 @@ def generate_histogram(datapoints, frame_idx, start = -0.03, end = 0.08, interva
     
 def get_trench_threshold(hist_data, hist_bins):
     max_idx = np.argmax(hist_data)
-    threshold = hist_bins[max_idx-4]
+    threshold = hist_bins[max_idx-15]
     return threshold
 
 def read_rosbag(filepath):
@@ -43,7 +42,7 @@ def read_rosbag(filepath):
         transformed_xyz -= mean_xyz
         data, bins = generate_histogram(transformed_xyz[:,2], idx, draw = False)
         trench_thresh = get_trench_threshold(data, bins)
-        heightmap = generate_heightmap(transformed_xyz, trench_thresh, mask = True)
+        heightmap, trench_thresh = generate_heightmap(transformed_xyz, trench_thresh, mask = True)
         utils.visualize_heightmap(heightmap, idx)        
         publish_transformed_cloud(transformed_xyz)
         utils.log_data(idx, trench_thresh)
@@ -56,16 +55,20 @@ def generate_heightmap(points, threshold, dim = 500, mask = False):
     z_co = points[:,2]
     x_co -= np.amin(x_co)
     y_co -= np.amin(y_co)
-    # min_val = np.amin(z_co)
-    # z_co -= min_val
-    # threshold -= min_val
+    min_val = np.amin(z_co)
+    z_co -= min_val
+    threshold -= min_val
+    threshold *= 10
+    z_co *= 10
     utils.publish_threshold_frame(threshold)
-    heightmap[x_co,y_co,0] = 1
+    heightmap[x_co,y_co,0] = z_co
+    heightmap[x_co,y_co,1] = z_co
+    heightmap[x_co,y_co,2] = z_co
     if mask:
         masked_x_co = x_co[z_co < threshold]
         masked_y_co = y_co[z_co < threshold]
         heightmap[masked_x_co, masked_y_co, 1] = 1    
-    return heightmap
+    return heightmap, threshold
 
 def publish_transformed_cloud(cloud_array, flag = True):
     header = std_msgs.msg.Header()
@@ -92,9 +95,9 @@ def transform_cloud(cloud_array, transformation_matrix = np.eye(4)):
 if __name__ == '__main__':
     rospy.init_node('cloud_processing_node')
     # bagfile_path = '../test_data/01_K10MINI_2020-05-14-16-23-00.bag'
-    # bagfile_path = '../test_data/02_K10MINI_2020-05-14-16-31-57.bag'
+    bagfile_path = '../test_data/02_K10MINI_2020-05-14-16-31-57.bag'
     # bagfile_path = '../test_data/03_K10MINI_2020-05-14-16-44-14.bag'
-    bagfile_path = '../test_data/04_K10MINI_2020-05-14-16-49-59.bag'
+    # bagfile_path = '../test_data/04_K10MINI_2020-05-14-16-49-59.bag'
     read_rosbag(bagfile_path)
     plt.ioff()
     print("Processing Complete")
